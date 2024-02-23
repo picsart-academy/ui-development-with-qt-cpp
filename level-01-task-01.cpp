35513750c99dd8017c9022a9dddd7cebbcd182a0
Picsart Aacademy <picsartaacademy@Picsarts-MacBook-Pro.local>
2024-02-23 15:39:48 +0400

    Init commit and add 01-01 task

---
+++ level-01-task-01.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Include the header file for input/output stream functionality
+
+int main() // Entry point of the program
+{
+    std::cout << "Hello, World!" << std::endl; // Output "Hello, World!" to the console
+    return 0; // Return 0 to indicate successful execution of the program
+}
